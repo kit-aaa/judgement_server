f035b306390b896e5b62ed3615dbb532d1f7a984
HelloKS <kqwe1859@gmail.com>
2022-06-06 18:55:13 +0000

    Gevent 사용

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
+from gevent.pywsgi import WSGIServer
 
 app = create_app()
 
 if __name__ == '__main__':
     print("KumohCheck judgement server!")
     
-    app.run()
+    server = WSGIServer(('0.0.0.0', 4000), app)
+    server.serve_forever()
